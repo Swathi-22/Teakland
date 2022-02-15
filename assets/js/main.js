36315021f612d1cbda8644a79084fa12528e0e36
swathi-22 <mkswathisuresh@gmail.com>
2022-02-15 17:02:37 +0530

    Teakland-form-creation

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+$(".nav a").on("click", function() {
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+  });
+
+
+
+$('.dasboard-hover').mouseenter(function(){
+    $('#dashboard-hover-image').attr('src','assets/image/1-2.png')
+})
+$('.dasboard-hover').mouseleave(function(){
+    $('#dashboard-hover-image').attr('src','assets/image/1-1.png')
+})
+
+
+
+$('.customer-hover').mouseenter(function(){
+    $('#customer-hover-image').attr('src','assets/image/2-1.png')
+})
+$('.customer-hover').mouseleave(function(){
+    $('#customer-hover-image').attr('src','assets/image/2-2.png')
+})
+
+
+
+$('.product-hover').mouseenter(function(){
+    $('#product-hover-image').attr('src','assets/image/3-1.png')
+})
+$('.product-hover').mouseleave(function(){
+    $('#product-hover-image').attr('src','assets/image/3-2.png')
+})
+
+
+
+$('.quotation-hover').mouseenter(function(){
+    $('#quotation-hover-image').attr('src','assets/image/0-1.png')
+})
+$('.quotation-hover').mouseleave(function(){
+    $('#quotation-hover-image').attr('src','assets/image/4-2.png')
+})
+
+
+
+$('.jobcard-hover').mouseenter(function(){
+    $('#jobcard-hover-image').attr('src','assets/image/4-1.png')
+})
+$('.jobcard-hover').mouseleave(function(){
+    $('#jobcard-hover-image').attr('src','assets/image/5-2.png')
+})
+
+
+
+$('.invoice-hover').mouseenter(function(){
+    $('#invoice-hover-image').attr('src','assets/image/5-1.png')
+})
+$('.invoice-hover').mouseleave(function(){
+    $('#invoice-hover-image').attr('src','assets/image/6-2.png')
+})
+
+
+
+$('.expense-hover').mouseenter(function(){
+    $('#expense-hover-image').attr('src','assets/image/6-1.png')
+})
+$('.expense-hover').mouseleave(function(){
+    $('#expense-hover-image').attr('src','assets/image/7-2.png')
+})
+
+
+
+$('.income-hover').mouseenter(function(){
+    $('#income-hover-image').attr('src','assets/image/7-1.png')
+})
+$('.income-hover').mouseleave(function(){
+    $('#income-hover-image').attr('src','assets/image/8-2.png')
+})
+
+
+
+$('.reports-hover').mouseenter(function(){
+    $('#reports-hover-image').attr('src','assets/image/8-1.png')
+})
+$('.reports-hover').mouseleave(function(){
+    $('#reports-hover-image').attr('src','assets/image/9-2.png')
+})
+
+
+
+$('.reports-hover').mouseenter(function(){
+    $('#reports-hover-image').attr('src','assets/image/8-1.png')
+})
+$('.reports-hover').mouseleave(function(){
+    $('#reports-hover-image').attr('src','assets/image/9-2.png')
+})
+
+
+
+$('.users-hover').mouseenter(function(){
+    $('#users-hover-image').attr('src','assets/image/9-1.png')
+})
+$('.users-hover').mouseleave(function(){
+    $('#users-hover-image').attr('src','assets/image/10-2.png')
+})
