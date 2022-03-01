69abc753a54a9dde195bc93bf6fffd4c82357095
swathi-22 <mkswathisuresh@gmail.com>
2022-03-01 17:03:40 +0530

    Teakland-Until-Reports

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,84 @@ $('.users-hover').mouseenter(function(){
 $('.users-hover').mouseleave(function(){
     $('#users-hover-image').attr('src','assets/image/10-2.png')
 })
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+//   =======graph========
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [{
+            label: 'Expense',
+            data: [12, 19, 3, 17, 28, 24, 7, 20, 50, 65, 55, 85],
+            backgroundColor: "#94CEE0",
+        }, {
+            label: 'Income',
+            data: [30, 29, 5, 5, 20, 3, 10, 15, 10, 6, 35, 60],
+            backgroundColor: "rgba(82, 159, 5, 0.5)"
+        }]
+    },
+
+    options: {
+        legend: {
+            display: true,
+            position: 'right',
+            maxSize: {
+                height: 200
+            },
+
+            labels: {
+                text: 'String',
+                boxWidth: 50,
+                boxHeight: '20',
+                boxshadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                fontColor: '#000',
+                fontFamily: '6px Montserrat',
+                label: 'Expense',
+                content: 'mureugan',
+
+            },
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                },
+                barPercentage: 0.7
+            }],
+            yAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                }
+            }],
+        }
+    },
+
+
+});
+
+
+$('[id=dropdown-arrow]').click(function(){
+    $('#drop-down-list').fadeToggle()
+})
